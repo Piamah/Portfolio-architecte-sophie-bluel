a0f7035759aff04c53f25caf6f20057308c239f7
Piama <annabellemoonflyff@gmail.com>
2024-04-24 18:38:18 +0200

    Ajout log in partiellement fonctionnel

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,27 +5,27 @@ const getWorks = ()=> {
     return fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((promise) => {
-        console.log(promise);
-    return promise;
+        console.log(promise)
+    return promise
     });
 };
 
 const afficheWorksHome = (works) => {
-    const elementGallery = document.querySelector('.gallery');
+    const elementGallery = document.querySelector('.gallery')
     elementGallery.innerHTML = ""
 
     works.forEach(work => {
-        const figure = document.createElement("figure");
-        const image = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
+        const figure = document.createElement("figure")
+        const image = document.createElement("img")
+        const figcaption = document.createElement("figcaption")
 
-        image.src = work.imageUrl;
-        image.alt = work.title;
-        figcaption.textContent = work.title;
+        image.src = work.imageUrl
+        image.alt = work.title
+        figcaption.textContent = work.title
 
-        figure.appendChild(image);
-        figure.appendChild(figcaption);
-        elementGallery.appendChild(figure);
+        figure.appendChild(image)
+        figure.appendChild(figcaption)
+        elementGallery.appendChild(figure)
     });
 }
 
@@ -37,53 +37,118 @@ getWorks()
 
 // Affichage et récupération Filtres
 
-async function getCategories (){
-    let response = await fetch ("http://localhost:5678/api/categories");
-    return await response.json();
-}
-
-// const getCategories = () => {
-//   return fetch("http://localhost:5678/api/categories")
-//       .then((res) => res.json())
-//       .then((categories) => {
-//         console.log(categories);
-//       return categories;
-//       });
-// };
+const getCategories = () => {
+  return fetch("http://localhost:5678/api/categories")
+      .then((res) => res.json())
+      .then((categories) => {
+        console.log(categories);
+      return categories;
+      });
+};
 
 
-async function showCategories() {
-    const filters = document.getElementsByClassName("filtres")
-    const categories = await getCategories()
-    console.log(categories)
-    categories.forEach((categorie) => {
-        const button= document.createElement("button")
-        button.textContent = categorie.name;
-        console.log(categorie.name)
-        // filters.appendChild(button)
+const filters = document.querySelector(".filtres")
+
+// On crée un bouton "Tous"
+const boutonTous = document.createElement("button")
+ // On lui met le texte "Tous"
+boutonTous.innerText = "Tous"
+// On met un addEventListener sur le clic de ce bouton "Tous"
+
+boutonTous.addEventListener("click", function() {
+    // On appelle la fonction getWorks()
+    getWorks()
+    // Dans le .then , on appelle la fonction afficheWorksHome en passant en paramètre les works obtenus
+    .then (works => {
+        afficheWorksHome(works)
     })
+});
+
+ // On l'insère dans filters
+filters.appendChild(boutonTous)
+
+/* On crée les boutons pour chaque catégorie dans la base de données */
+
+const getWorksFiltered = (id) => {
+    return fetch("http://localhost:5678/api/works")
+        .then((res) => res.json())
+        .then((works) => {
+            const worksFiltered = works.filter (function(work) {
+                return work.category.id === id
+            })
+        return worksFiltered
+        })
+  }
+
+// On appelle getCategories()
+getCategories()
+// Dans le .then, on boucle sur chaque catégorie
+  .then(categories => {
+    // Pour chaque catégorie trouvée, on crée un bouton
+    categories.forEach(categorie => {
+        const bouton = document.createElement("button")
+        // On lui met comme texte le nom de la catégorie
+        bouton.textContent = categorie.name
+
+        filters.appendChild(bouton)
+        // On met un addEventListener sur le clic de ce bouton
+        bouton.addEventListener("click", function(){
+            // On appelle la fonction getWorksFiltered en passant en paramètre le nom de la catégorie
+            getWorksFiltered(categorie.id)
+            // Dans le .then , on appelle la fonction afficheWorksHome en passant en paramètre les works obtenus
+            .then(works => {
+                afficheWorksHome(works)
+            })
+        })
+      })
+    })
+
+// Creation du login
+const loginIn = (form)=> {
+    fetch ('http://localhost:5678/api/users/login', {
+        method: "POST",
+        headers:  {"Content-Type": "application/json" },
+        body: JSON.stringify({email, password}),
+    })
+
+    //   // Définition des constantes 
+    const loginForm = document.getElementById("loginForm")
+    const emailInput = document.getElementById("emailUser")
+    const passwordInput = document.getElementById("passwordUser")
+    //   loginForm.addEventListener('submit', event => {
+    //     event.preventDefault();
+    function login () {
+        loginForm.addEventListener("submit", event => {
+            event.preventDefault()
+
+        //     // Récupération des valeurs saisies dans username et password
+        const email = emailInput.value
+        const password = passwordInput.value
+        
+            if(email === "" || password === "") {
+                alert("Merci de remplir les champs obligatoires")
+            
+            }else{
+            loginIn()
+            }
+
+        })
+
+    }
 }
 
-showCategories()
 
+//     // Si un des deux champs est vide , on arrête tout et on alerte
 
-// const afficheFiltres = (filtres) => {
-//   const elementFiltres = document.querySelector(".filtres");
-//   elementFiltres.innerHTML = "";
+//     // Sinon on appelle la route de login en faisant attention
+// à la manière dont on passe les infos
 
-//   filtres.forEach(filtre => {
-//     const button = document.createElement('button');
-//     button.id = filtre.id;
-//     button.textContent = filtre.label;
-//     button.addEventListener("click", () => {
-//       // Action à effectuer lorsqu'un filtre est cliqué, par exemple filtrer les projets en fonction de ce filtre
-//       filterWorksByFiltre(filtre.id);
-//     });
-//     elementFiltres.appendChild(button);
-//   });
-// }
+//     // S'il y a un soucis avec la réponse de l'API, on prévient
+// l'utilisateur
+//     // Si tout s'est bien passé, on récupère le token renvoyé
+// par l'API, on le stocke dans le localstorage, et on redirige
+// vers index.html
 
-// getFiltres()
-//     .then(filtres => {
-//         afficheFiltres(filtres);
-//     });
+// 
+
+  

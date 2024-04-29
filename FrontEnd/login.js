096823b556443b7c7e2d830e4f142f1a6d09a09b
Piama <annabellemoonflyff@gmail.com>
2024-04-29 21:50:38 +0200

    Correction de la partie login fonctionnelle

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,63 +1,40 @@
- //   // Définition des constantes 
-let emailInput = document.getElementById("emailUser")
-let passwordInput = document.getElementById("passwordUser")
-let email = emailInput.value
-let password = passwordInput.value
-let message = []
-console.log(email)
-// Mettre les autres valeurs ici ? .log => 0
+ //   // Définition des constantes
+const form = document.querySelector("form")
+let token = localStorage.getItem("token")
 
-const login = (form)=> {
-    fetch ('http://localhost:5678/api/users/login', {
-        method: "POST",
-        headers:  {
-            "Content-Type": "application/json"},
-            // "Authorization": "Bearer "},
-        body: JSON.stringify({email, password}),
-    })
-        form.addEventListener("submit", event => {
-            event.preventDefault()
-
-        //     // Récupération des valeurs saisies dans username et password
-
-            if(email === null || email === '' && password ==null || password === '') {
-                message.push("Erreur dans l’identifiant ou le mot de passe")
-                return
-
-            }else{
-
-                login()
+// On ecoute l'event lorsque on soumet le formulaire
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    // Récupération des valeurs saisies dans username et password
+        let form = { 
+            email:  document.getElementById("email"),
+            password: document.getElementById("password"),
             }
-            
-        })
-}
 
+fetch ('http://localhost:5678/api/users/login', {
+    method: "POST",
+    headers:  {
+        "Content-Type": "application/json"},
+        // "Authorization": "Bearer "},
+    body: JSON.stringify({email: form.email.value, password: form.password.value}),
+})
+ // Si un des deux champs est vide , on arrête tout et on alerte 
+ // Si un des deux champs = une erreur, alors ... -->
+.then((response) => {
+    if (response.status !== 200) {
+        alert("Erreur dans l’identifiant ou le mot de passe")
+ // Sinon on appelle la route de login 
+    } else {
+        response.json()
+        .then ((data) => {
+            sessionStorage.setItem("token", data.token)
+            window.location.replace("index.html")
+            })
+        }
+    })
+})
+
+//Info Token
 localStorage.setItem("userId", 1)
 localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcxNDE0ODA4OSwiZXhwIjoxNzE0MjM0NDg5fQ.5wY-jxs-iehNYrqT60Tj4H93BD9RQxlKWq7FCkvuk1U")
 
-// Quand les 2 sont incorrects : Erreur dans l’identifiant ou le mot de passe
-
-//     // Si un des deux champs est vide , on arrête tout et on alerte
-
-//     // Sinon on appelle la route de login en faisant attention
-// à la manière dont on passe les infos
-
-//     // S'il y a un soucis avec la réponse de l'API, on prévient
-// l'utilisateur
-//     // Si tout s'est bien passé, on récupère le token renvoyé
-// par l'API, on le stocke dans le localstorage, et on redirige
-// vers index.html
-
-// 
-
-// En suivant la maquette, intégrer le formulaire de connexion au site. 
-
-// Lorsque le couple identifiant et mot de passe n’est pas bon pour se
-//  connecter il faut afficher le message d’erreur: 
-
-// “Erreur dans l’identifiant ou le mot de passe”
-
-// Lorsque le couple identifiant et mot de passe est correct. 
-
-// Alors il faut rediriger vers la page du site avec cette fois 
-// ci des boutons d’actions pour éditer le site.
